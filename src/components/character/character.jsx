103d972d6943d9ea491cee9e08533d003c67c7dc
Agustin <agus.ammazzagatti@gmail.com>
2021-12-05 21:42:15 -0300

    nuevo commit 1

--- src/components/character/character.jsx
+++ src/components/character/character.jsx
@@ -4,24 +4,53 @@ import { useParams } from "react-router-dom";
 
 export const Character = () => {
   const { id } = useParams();
-  const [powerstats, setPowerstats] = useState([]);
+  const [detailHero, setDetailHero] = useState([]);
 
   useEffect(() => {
-    axios.get(`/10223769438797732/${id}/powerstats`).then((res) => {
+    axios.get(`/10223769438797732/${id}`).then((res) => {
       const data = res.data;
 
       console.log(data);
 
-      setPowerstats(data);
+      setDetailHero(data);
     });
   }, []);
 
   return (
     <div>
-      <h1>{powerstats.speed}</h1>
+      <h1>{detailHero.work.occupation}</h1>
+      {/*       <div>
+        {Object.values(detailHero).filter((character) => (character.id = id))}
+      </div> */}
+    </div>
+  );
+};
+
+/* import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export const Character = () => {
+  const { id } = useParams();
+  const [detailHero, setDetailHero] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/10223769438797732/${id}/detailHero`).then((res) => {
+      const data = res.data;
+
+      console.log(data);
+
+      setDetailHero(data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>{detailHero.speed}</h1>
       <div>
-        {Object.values(powerstats).filter((character) => (character.id = id))}
+        {Object.values(detailHero).filter((character) => (character.id = id))}
       </div>
     </div>
   );
 };
+ */
