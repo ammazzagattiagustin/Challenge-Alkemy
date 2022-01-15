8c7e0959793e353f3c7d28117b6f3d4aff2f3034
Agustin <agus.ammazzagatti@gmail.com>
2022-01-15 18:32:30 -0300

    navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -13,21 +13,17 @@ export const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
         <div className="container-fluid">
-          <a href="/" className="navbar-brand">SuperHero</a>
+          <NavLink to="/" className="navbar-brand">SuperHero</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <NavLink to={"/home"}>
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
+              <NavLink to="/home" className="nav-item nav-link active">
+                Home
               </NavLink>
-              <NavLink to={"/search"}>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">Search</a>
-                </li>
+              <NavLink to="/search" className="nav-item nav-link active">
+                Search
               </NavLink>
             </ul>
             <form className="d-flex">
