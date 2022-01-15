6790ae759aae6ef5b7fbabe368bc74ad954ed076
Agustin <agus.ammazzagatti@gmail.com>
2022-01-15 18:17:18 -0300

    add navigate in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Login } from "./components/login/login";
 import { Home } from "./components/home/home";
 import { Search } from "./components/search/search";
 import { Character } from "./components/character/character";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -13,6 +13,10 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/character/:id" element={<Character />} />
+          <Route
+            path="*"
+            element={<Navigate to="/" />}
+          />
         </Routes>
       </BrowserRouter>
     </>
